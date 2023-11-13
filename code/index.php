f9617d08412965e1c342d4b3883a87a5aed960dc
SeanNeerings <seanneerings1@gmail.com>
2023-11-13 11:21:47 +0100

    pagination
    pagination

--- code/index.php
+++ code/index.php
@@ -1,54 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/css/style.css">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Document</title>
 </head>
+
 <body>
-  <div class="container">
-    <div class="searchbar">
-      <form action="" method="post">
-      <input class="search" type="text" name="search" />
-      <input type="submit" value="Search" />
-      </form>
-      </div>
-  <?php
-include 'dbconn.php';
-include 'search.php';
+    <div class="container">
+        <div class="searchbar">
+            <form action="" method="get">
+                <input class="search" type="text" name="search" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>" />
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+        <?php
+        include 'dbconn.php';
+        include 'search.php';
 
-try {
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if (isset($_REQUEST['search'])) {
-        $search = '%' . $_REQUEST['search'] . '%';
-        $stmt = $conn->prepare("SELECT project_id, project_name, project_about, type_project FROM projects WHERE project_name LIKE :search OR type_project LIKE :search");
-        $stmt->bindParam(':search', $search, PDO::PARAM_STR);
-    } else {
-        $stmt = $conn->prepare("SELECT project_id, project_name, project_about, type_project FROM projects ");
-    }
+            $results_per_page = 5; 
 
-    $stmt->execute();
+            $stmt = $conn->prepare("SELECT COUNT(*) AS total FROM projects");
+            $stmt->execute();
+            $total_results = $stmt->fetch(PDO::FETCH_ASSOC)['total'];
+            $total_pages = ceil($total_results / $results_per_page);
 
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-    foreach ($stmt->fetchAll() as $k => $v) {
-        echo "<div class='project_box'>";
-        echo "<a href='detail.php?project=" . $v['project_id'] . "'><h1 class='title'>$v[project_name] </h1> </a>";
-        echo "<p class='type'> $v[type_project]</p>";
-        echo "<p class='about'> $v[project_about] </p>";
-        echo "</div>";
-        echo "<br>";
-    }
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
+            $start_index = ($current_page - 1) * $results_per_page;
 
-$conn = null;
-?>
+            if (isset($_GET['search'])) {
+                $search = '%' . $_GET['search'] . '%';
 
-</div>
+                $stmt = $conn->prepare("SELECT project_id, project_name, project_about, type_project FROM projects WHERE project_name LIKE :search OR type_project LIKE :search LIMIT $start_index, $results_per_page");
+                $stmt->bindParam(':search', $search, PDO::PARAM_STR);
+            } else {
+                $stmt = $conn->prepare("SELECT project_id, project_name, project_about, type_project FROM projects LIMIT $start_index, $results_per_page");
+            }
+
+            $stmt->execute();
+
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            foreach ($stmt->fetchAll() as $k => $v) {
+                echo "<div class='project_box'>";
+                echo "<a href='detail.php?project=" . $v['project_id'] . "'><h1 class='title'>$v[project_name] </h1> </a>";
+                echo "<p class='type'> $v[type_project]</p>";
+                echo "<p class='about'> $v[project_about] </p>";
+                echo "</div>";
+                echo "<br>";
+            }
+
+            for ($page = 1; $page <= $total_pages; $page++) {
+                echo "<a href='?page=$page" . (isset($_GET['search']) ? "&search={$_GET['search']}" : "") . "'>$page</a> ";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        $conn = null;
+        ?>
+    </div>
 </body>
-</html>
 
+</html>
